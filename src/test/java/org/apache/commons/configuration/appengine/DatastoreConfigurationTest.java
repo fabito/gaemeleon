c3a297a5482c22106e900172e7d3aee576e37857
fabito <fuechi@ciandt.com>
2014-03-13 10:49:27 -0300

    Fixing broken test

--- src/test/java/org/apache/commons/configuration/appengine/DatastoreConfigurationTest.java
+++ src/test/java/org/apache/commons/configuration/appengine/DatastoreConfigurationTest.java
@@ -1,9 +1,8 @@
 package org.apache.commons.configuration.appengine;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNull.nullValue;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsNull.nullValue;
 
 import java.util.Iterator;
 import java.util.Map;
@@ -22,39 +21,35 @@ import com.google.common.collect.ImmutableMap;
 
 public class DatastoreConfigurationTest {
 
-    private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
-            new LocalDatastoreServiceTestConfig());
-	
-    private DatastoreConfiguration config;
-    private DatastoreService datastoreService;
-    
-    private Map<String, String> initialConfiguration = ImmutableMap.<String, String>of(
-    		"p1", "v1",
-    		"p2", "v2",
-    		"p3", "v3",
-    		"p4", "v4"
-    );
-    
-    @Before
-    public void setup() {
-        helper.setUp();
-        datastoreService = DatastoreServiceFactory.getDatastoreService();
-        addConfigurations(initialConfiguration);
-        config = new DatastoreConfiguration(datastoreService);
-    }
-    
-    @After
-    public void tearDown() {
-        helper.tearDown();
-    }
-    
+	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
+			new LocalDatastoreServiceTestConfig());
+
+	private DatastoreConfiguration config;
+	private DatastoreService datastoreService;
+
+	private Map<String, String> initialConfiguration = ImmutableMap
+			.<String, String> of("p1", "v1", "p2", "v2", "p3", "v3", "p4", "v4");
+
+	@Before
+	public void setup() {
+		helper.setUp();
+		datastoreService = DatastoreServiceFactory.getDatastoreService();
+		addConfigurations(initialConfiguration);
+		config = new DatastoreConfiguration(datastoreService);
+	}
+
+	@After
+	public void tearDown() {
+		helper.tearDown();
+	}
+
 	@Test
 	public void existentPropertyShouldReturnRightValue() {
 		assertThat(config.getString("p1"), is("v1"));
 		assertThat(config.getString("p2"), is("v2"));
 		assertThat(config.getString("p3"), is("v3"));
 	}
-	
+
 	@Test
 	public void nonExistentPropertyShouldReturnNull() {
 		assertThat(config.getString("p10"), nullValue());
@@ -62,21 +57,17 @@ public class DatastoreConfigurationTest {
 
 	@Test
 	public void getKeysShouldReturnRightValue() {
-		Iterator<String> iterator = config.getKeys();
-		int counter = 0;
-		while( iterator.hasNext() ) {
-			  String e = (String) iterator.next() ;
-			  assertThat(initialConfiguration.keySet(), hasItem(e));
-			  counter++;
+		for (Iterator<String> it1 = config.getKeys(), it2 = initialConfiguration
+				.keySet().iterator(); it1.hasNext() && it2.hasNext();) {
+			assertThat(it1.next(), is(it2.next()));
 		}
-		assertThat(counter, is(initialConfiguration.size()));
 	}
 
 	@Test
 	public void isEmptyShouldReturnFalse() {
 		assertThat(config.isEmpty(), is(false));
 	}
-	
+
 	@Test
 	public void addPropertyDirect() {
 		String key = "ppp";
@@ -85,9 +76,8 @@ public class DatastoreConfigurationTest {
 		config.addPropertyDirect(key, value);
 		assertThat(config.getString(key), is(value));
 	}
-	
 
-	//@Test
+	// @Test
 	public void isEmptyShouldReturnTrue() {
 		assertThat(config.isEmpty(), is(true));
 	}
@@ -96,22 +86,24 @@ public class DatastoreConfigurationTest {
 	public void containsKey() {
 		assertThat(config.containsKey("p1"), is(true));
 	}
-	
+
 	@Test
 	public void containsKeyShouldReturnFalse() {
 		assertThat(config.containsKey("53454353"), is(false));
 	}
-	
+
 	private void addConfigurations(Map<String, String> props) {
-		for(Entry<String, String> e : props.entrySet()) {
+		for (Entry<String, String> e : props.entrySet()) {
 			addConfiguration(e.getKey(), e.getValue());
 		}
 	}
-	
-    private void addConfiguration(String key, String value) {
-        Entity configurationEntity = new Entity(DatastoreConfiguration.DEFAULT_ENTITY_KIND, key);
-        configurationEntity.setUnindexedProperty(DatastoreConfiguration.DEFAULT_PROPERTY_VALUE, value);
-        datastoreService.put(configurationEntity);
-    }
+
+	private void addConfiguration(String key, String value) {
+		Entity configurationEntity = new Entity(
+				DatastoreConfiguration.DEFAULT_ENTITY_KIND, key);
+		configurationEntity.setUnindexedProperty(
+				DatastoreConfiguration.DEFAULT_PROPERTY_VALUE, value);
+		datastoreService.put(configurationEntity);
+	}
 
 }
