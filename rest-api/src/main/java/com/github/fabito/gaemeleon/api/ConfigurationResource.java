5ccf1a895f12a465da8186e687b83c53724f19ca
fabito <fuechi@ciandt.com>
2014-04-12 22:58:38 -0300

    removing unused code

--- rest-api/src/main/java/com/github/fabito/gaemeleon/api/ConfigurationResource.java
+++ rest-api/src/main/java/com/github/fabito/gaemeleon/api/ConfigurationResource.java
@@ -1,19 +1,16 @@
 package com.github.fabito.gaemeleon.api;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import org.apache.commons.configuration.Configuration;
@@ -52,14 +49,14 @@ public class ConfigurationResource {
 		return Response.ok(property).build();
 	}
 	
-	@PUT
-	@Path("/{propertyName}")
-	public Response put(@PathParam("propertyName") String propertyName) {
-		Object value = null;
-		configuration.addProperty(propertyName, value);
-		Object values = null;
-		UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
-		URI location = uriBuilder.build(values);
-		return Response.created(location).build();
-	}
+//	@PUT
+//	@Path("/{propertyName}")
+//	public Response put(@PathParam("propertyName") String propertyName) {
+//		Object value = null;
+//		configuration.addProperty(propertyName, value);
+//		Object values = null;
+//		UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+//		URI location = uriBuilder.build(values);
+//		return Response.created(location).build();
+//	}
 }
--- rest-api/src/main/java/com/github/fabito/gaemeleon/api/Property.java
+++ rest-api/src/main/java/com/github/fabito/gaemeleon/api/Property.java
@@ -20,16 +20,9 @@ public class Property {
 		return key;
 	}
 
-	public void setKey(String key) {
-		this.key = key;
-	}
 
 	public Object getValue() {
 		return value;
-	}
-
-	public void setValue(Object value) {
-		this.value = value;
 	}
 
 }
